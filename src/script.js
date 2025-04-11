0ac52e734b1f5977bb6efd4acf16bcbba8aaaaff
yousefbahutair <yousefnas06@gmail.com>
2025-04-11 22:40:49 +0400

    feat: use shuffle function in the new function stepThroughCell

--- src/script.js
+++ src/script.js
@@ -1,4 +1,13 @@
-const { Engine, Render, Runner, World, Bodies} = Matter;
+const {
+     Engine, 
+     Render, 
+     Runner, World, 
+     Bodies
+     } = Matter;
+
+const cells = 3;
+let height  = 600;
+let width   = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,8 +15,9 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes : true,
+        width: width,
+        height: height
     }
 });
 Render.run(render);
@@ -15,13 +25,73 @@ Runner.run(Runner.create(), engine);
 
 //Walls 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 800, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 800, { isStatic: true }),
+    Bodies.rectangle(width * 0.5, 0, height, 40, { isStatic: true }),
+    Bodies.rectangle(width * 0.5, height, height, 40, { isStatic: true }),
+    Bodies.rectangle(width, height * 0.5, 40, height, { isStatic: true }),
+    Bodies.rectangle(0, height * 0.5, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
-//Rectangle
+//Maze Generation
 
-World.add(world, Bodies.rectangle(200, 200, 60, 60));
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while(counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+}
+const grid = Array(cells)
+.fill(null)
+.map(() => Array(cells).fill(false));
+
+
+const verticals = Array(cells)
+.fill(null)
+.map(() => Array(cells - 1).fill(false));
+
+
+const horizontals = Array(cells - 1)
+.fill(null)
+.map(() => Array(cells).fill(false));
+
+const startRow =  Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+
+const stepThroughCell = (row, column) => {
+    // If i have visited the cell at [row, column], then return
+    if(grid[row][column]) {
+        return;
+    }
+
+    // Mark this cell as visited
+    grid[row][column] = true;
+
+    // Assemble randomly-ordered list of neighbors
+    const neighbors = shuffle([
+        [row - 1, column],
+        [row, column + 1],
+        [row + 1, column],
+        [row, column - 1]
+    ]);
+    console.log(neighbors);
+
+    // For each neighbor....
+
+    // See if that neighbor is out of bounds
+
+    // If we have visited that neighbor, continue to next neighbor
+
+    // Remove a wall from either horizontals
+
+    // Visit that next cell
+}
+stepThroughCell(1,1)
